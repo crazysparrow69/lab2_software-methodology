b6cc4b65112f570316863b665be9699439e25a40
den <denissytnikmacho@gmail.com>
2023-03-10 18:14:01 +0200

    feature: converted linked list class into a real linked list

--- index.js
+++ index.js
@@ -5,93 +5,257 @@ class Node {
 }
 
 class LinkedList {
-  #storage = [];
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
   length() {
-    return this.#storage.length;
+    if (!this.tail) {
+      return 0;
+    }
+
+    return this.tail.index;
   }
 
   append(data) {
+    if (typeof data !== "string") return;
+
     const node = new Node(data);
-    this.#storage.push(node);
+
+    if (!this.head) {
+      this.head = node;
+      node.index = 0;
+    }
+
+    if (this.tail) {
+      this.tail.next = node;
+      node.index = this.tail.index + 1;
+    }
+
+    if (this.length() > 1) {
+      node.next = this.head;
+    }
+
+    this.tail = node;
   }
 
   insert(data, index) {
-    if (index < 0 || this.#storage[index] === undefined) {
-      throw new Error('Incorrect index');
+    if (index < 0 || index > this.tail.index) {
+      throw new Error("Incorrect index");
     }
 
     const node = new Node(data);
-    this.#storage.splice(index, 0, node);
+
+    if (index === 0) {
+      node.next = this.head;
+      node.index = 0;
+      this.head = node;
+      this.tail.next = this.head;
+    } else {
+      const found = this.get(index - 1);
+      node.next = found.next;
+      found.next = node;
+      node.index = found.index + 1;
+    }
+
+    this.#correctIndex(node.next);
   }
 
   delete(index) {
-    if (index < 0 || this.#storage[index] === undefined) {
-      throw new Error('Incorrect index');
+    if (index < 0 || index > this.tail.index || typeof(index) !== "number") {
+      throw new Error("Incorrect index");
     }
 
-    const elem = this.get(index);
-    this.#storage.splice(index, 1);
-    return elem;
+    let deletedElem = null;
+
+    if (this.length() === 0) {
+      return;
+    } else if (this.length() === 1) {
+      deletedElem = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      deletedElem = this.get(index);
+
+      if (deletedElem.index === 0) {
+        this.head = deletedElem.next;
+        this.tail.next = this.head;
+
+        this.#correctIndex(this.head, "decreasing");
+      } else if (deletedElem.index === this.tail.index) {
+        const prevElem = this.get(index - 1);
+        prevElem.next = this.head;
+      } else {
+        const prevElem = this.get(index - 1);
+        const nextElem = this.get(index + 1);
+        prevElem.next = nextElem;
+  
+        this.#correctIndex(nextElem, "decreasing");
+      }
+    }
+
+    return deletedElem;
   }
 
   deleteAll(data) {
-    for (let i = 0; i < this.length(); i++) {
-      if (this.#storage[i].data === data) {
-        this.delete(i);
+    if (typeof data !== "string" || this.length() === 0) {
+      return;
+    } else if (this.length() === 1) {
+      if (this.head.data === data) {
+        this.delete(this.head.index);
+      }
+    } else {
+      let current = this.head;
+      let index = 0;
+      while (current.index >= index) {
+        if (current.data === data) {
+          this.delete(current.index);
+        }
+  
+        index = current.index;
+        current = current.next;
       }
     }
   }
 
   get(index) {
-    if (index < 0 || this.#storage[index] === undefined) {
-      throw new Error('Incorrect index');
+    if (index < 0 || index > this.tail.index || this.length() === 0 ) {
+      throw new Error("Incorrect index");
+    } else if (this.length() === 1) {
+        return this.head;
+    } else {
+      let current = this.head;
+      let i = 0;
+      while (current.index >= i) {
+        if (current.index === index) {
+          return current;
+        }
+        i = current.index;
+        current = current.next;
+      }
     }
-    return this.#storage[index];
   }
 
   clone() {
     const clonedList = new LinkedList();
-    for (const elem of this.#storage) {
-      clonedList.append(elem.data);
+
+    if (this.length() === 1) {
+      clonedList.append(this.head.data);
+    } else if (this.length() > 1) {
+      let current = this.head;
+      let index = 0;
+      while (current.index >= index) {
+        clonedList.append(current.data);
+        index = current.index;
+        current = current.next;
+      }
     }
+
     return clonedList;
   }
 
   reverse() {
-    this.#storage.reverse();
+    if (this.length() < 2) return;
+
+    const length = this.length();
+    for (let i = 0; i < length - 1; i++) {
+      this.insert(this.delete(this.tail.index).data, i);
+    }
   }
 
   findFirst(data) {
-    for (let i = 0; i < this.length(); i++) {
-      if (this.#storage[i].data === data) {
-        return i;
+    if (typeof data !== "string") return;
+
+    let current = this.head;
+    let index = 0;
+
+    while (current.index >= index) {
+      if (current.data === data) {
+        return current.index;
       }
+      index = current.index;
+      current = current.next;
     }
+
     return -1;
   }
 
   findLast(data) {
-    for (let i = this.length() - 1; i >= 0; i--) {
-      if (this.#storage[i].data === data) {
-        return i;
+    let current = this.head;
+    let index = 0;
+    let foundElementIndex = null;
+
+    while (current.index >= index) {
+      if (current.data === data) {
+        foundElementIndex = current.index;
       }
+
+      index = current.index;
+      current = current.next;
     }
-    return -1;
+
+    if (!foundElementIndex) {
+      return -1;
+    } else {
+      return foundElementIndex;
+    }
   }
 
   clear() {
-    this.#storage.splice(0, this.length());
+    this.head = null;
+    this.tail = null;
   }
 
   extend(list) {
-    for (const elem of list.toArray()) {
-      this.#storage.push(elem);
+    let current = list.head;
+    let index = 0;
+
+    while (current.index >= index) {
+      this.append(current.data);
+      index = current.index;
+      current = current.next;
     }
   }
 
   toArray() {
-    return this.#storage;
+    const output = [];
+
+    if (this.length() === 1) {
+      output.push(this.head);
+    } else if (this.length() > 1) {
+      let current = this.head;
+      let index = 0;
+
+      while (current.index >= index) {
+        output.push(current);
+        index = current.index;
+        current = current.next;
+      }
+    }
+    
+    return output;
+  }
+
+  #correctIndex(startElem, order = "increasing") {
+    let current = startElem;
+    let i = 0;
+
+    if (order === "increasing") {
+      while (current.index >= i) {
+        current.index++;
+        i = current.index;
+        current = current.next;
+      }
+    } else if (order === "decreasing") {
+      while (current.index >= i) {
+        current.index--;
+        i = current.index;
+        current = current.next;
+      }
+    } else {
+      return;
+    }
   }
 }
 
